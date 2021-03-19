717232d687480c6ca801265c7e249c6c7a2756b8
David <ddsabiro@gmail.com>
2021-03-19 11:32:06 +0700

    :sparkles: New Sassy testcases

---
+++ testcase_sassy/testcase_recurring_check.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from decouple import config
+import unittest, time, re
+
+Email = config("SASSY_EMAIL", cast=str)
+Password = config("SASSY_PASSWORD", cast=str)
+Filename = "test"
+MemberID = config("SASSY_MEMBER_ID", cast=str)
+MemberEmail = config("SASSY_MEMBER_EMAIL", cast=str)
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Edge(config("DRIVER_PATH", cast=str))
+        # self.driver = webdriver.Chrome(config("DRIVER_PATH", cast=str))
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("https://staging.superyou.co.id/sassy/login") # Website Link
+
+        # Login
+        driver.find_element_by_xpath("/html/body/div/div/form/div[1]/input").click() # Email field
+        driver.find_element_by_xpath("/html/body/div/div/form/div[1]/input").clear()
+        driver.find_element_by_xpath("/html/body/div/div/form/div[1]/input").send_keys(Email) # Input email
+        driver.find_element_by_xpath("/html/body/div/div/form/div[2]/input").clear()
+        driver.find_element_by_xpath("/html/body/div/div/form/div[2]/input").send_keys(Password) # Input password
+        
+        driver.find_element_by_xpath("//button[@type='submit']").click() # Login button
+        time.sleep(2)
+
+        driver.find_element_by_xpath("//div[@id='nova']/div/div/h3[2]/span").click() # Resources
+        driver.find_element_by_xpath("//*[@id='nova]/div/div[1]/ul[13]/li[1]/a").click() # Recurring Check
+        time.sleep(1)
+        driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/fade-transition/div/div/form/div[1]/div[1]/div[2]/input").click() # Member ID field
+        driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/fade-transition/div/div/form/div[1]/div[1]/div[2]/input").clear()
+        driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/fade-transition/div/div/form/div[1]/div[1]/div[2]/input").send_keys(MemberID) # Input Member ID
+        driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/fade-transition/div/div/form/div[1]/div[2]/div[2]/input").click() # Member Email field
+        driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/fade-transition/div/div/form/div[1]/div[2]/div[2]/input").clear()
+        driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/fade-transition/div/div/form/div[1]/div[2]/div[2]/input").send_keys(MemberEmail) # Input Member Email
+        time.sleep(1)
+        driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/fade-transition/div/div/form/div[2]/a").click() # Click Check button
+        time.sleep(300)
+
+        driver.close()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
